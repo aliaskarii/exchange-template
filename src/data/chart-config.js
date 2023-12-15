80e4e85058c36baab964a886f8f0d4a94473d749
aliaskari76 <askari1791@hotmail.com>
2023-12-15 15:02:29 -0500

    [fix] symbol price list

---
+++ src/data/chart-config.js
@@ -0,0 +1,61 @@
+
+
+const chartdata = {
+    type: 'line',
+    height: 100,
+    width: 250,
+    options: {
+        chart: {
+            toolbar: {
+                show: false,
+            },
+        },
+        yaxis: {
+            labels: {
+                show: false
+            },
+        },
+        grid: {
+            show: false,
+            xaxis: {
+                lines: {
+                    show: false,
+                },
+            },
+            padding: {
+                top: 0,
+                right: 0,
+            },
+        },
+        fill: {
+            opacity: 0.5,
+        },
+        tooltip: {
+            theme: 'dark',
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        colors: ['#5FBC69'],
+        stroke: {
+            lineCap: 'round',
+        },
+        markers: {
+            show:false,
+            size: 1,
+        },
+        xaxis: {
+            axisTicks: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            labels: {
+                show: false,
+            },
+            name: 'chart',
+        },
+    },
+}
+export default chartdata
--- src/data/currencies.js
+++ src/data/currencies.js
@@ -1,173 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import { Paper, Pagination, Stack } from '@mui/material';
-import Chart from 'react-apexcharts'
-import Skeleton from '@mui/material/Skeleton';
+import React, { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Paper, Pagination, Stack } from "@mui/material";
+import Chart from "react-apexcharts";
+import Skeleton from "@mui/material/Skeleton";
+import chartdata from "../data/chart-config";
+
+async function fetchSymbolPrices(name) {
+  const response = await fetch(
+    `https://api.binance.us/api/v3/klines?interval=1d&symbol=${name}&limit=50`
+  );
+  const symbolKLine = await response.json();
+  const symbolPrices = [];
+  for (let index = 0; index < symbolKLine.length; index++) {
+    symbolPrices.push(symbolKLine[index][4]);
+  }
+  return { name: name, price: symbolPrices };
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
 
 export default function Orders() {
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [symbolprice, setSymbolPrice] = useState([]);
+  const rowsPerPage = 5;
 
-    const series = [
-        {
-            name: 'ROI',
-            data: [1123, 123, 43, 5, 546, 657, 6, 87]
-        },
-    ]
+  const handleChangePage = (_, newPage) => {
+    setPage(newPage);
+  };
 
-    const chartdata = {
-        type: 'line',
-        height: 80,
-        width: 200,
-        options: {
-            chart: {
-                toolbar: {
-                    show: false,
-                },
-            },
-            yaxis: {
-                labels: {
-                    show: false
-                },
-            },
-            grid: {
-                show: false,
-                borderColor: '#dddddd',
-                strokeDashArray: 0,
-                xaxis: {
-                    lines: {
-                        show: false,
-                    },
-                },
-                padding: {
-                    top: 5,
-                    right: 20,
-                },
-            },
-            fill: {
-                opacity: 0.8,
-            },
-            tooltip: {
-                theme: 'dark',
-            },
-            dataLabels: {
-                enabled: false,
-            },
-            colors: ['#5FBC69'],
-            stroke: {
-                lineCap: 'round',
-            },
-            markers: {
-                size: 5,
-            },
-            xaxis: {
-                axisTicks: {
-                    show: false,
-                },
-                axisBorder: {
-                    show: false,
-                },
-                labels: {
-                    show: false,
-                },
-                name: 'income',
-            },
-        },
-    }
+  const symbols = [
+    "BTCUSDT",
+    "BNBUSDT",
+    "LTCUSDT",
+    "XRPUSDT",
+    "STMXUSDT",
+    "ETHUSDT",
+    "DOGEUSDT",
+  ];
 
-    const [loading, setloading] = useState(true);
-    const [page, setPage] = useState(0);
-    const [symbolprice, setsymbolprice] = useState([])
-    const rowperpage = 6
+  useEffect(() => {
+    setLoading(true);
+    Promise.all(
+      symbols
+        .slice((page - 1) * rowsPerPage, page * rowsPerPage)
+        .map((name) => fetchSymbolPrices(name))
+    ).then((res) => {
+      setSymbolPrice(res);
+      setLoading(false);
+    });
+  }, [page]);
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleSymbolprice = (prop) => {
-        const array = symbolprice
-        array.push(prop)
-        setsymbolprice(array)
-    }
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-    }
-    // "symbol": "BTCUSDT",
-    // "interval": "1M",
-    // "startTime": "2021-11",
-    // "endTime": "2023-11",
-    // "limit": 500,
-    const symbols = ['BTCUSDT', 'BNBUSDT', 'LTCUSDT', 'XRPUSDT', 'STMXUSDT']
-    useEffect(() => {
-        async function fetchCurrencyData(name) {
-            const response = await fetch(`https://api.binance.us/api/v3/klines?interval=1M&symbol=${name}&limit=10`)
-            const currencyData = await response.json()
-            const array = []
-            for (let index = 0; index < currencyData.length; index++) {
-                array.push(currencyData[index][4])
-            }
-            handleSymbolprice({ name: name, price: array })
-        }
-        if (symbols) {
-            let requests = symbols.map(name => fetchCurrencyData(name))
-            Promise.all(requests)
-                .then(
-                    setloading(false),
-                );
-            console.log(loading)
-        }
-        console.log(symbolprice)
-    }, [])
-
-    if (loading) {
-        return (
-            <>
-                <Skeleton sx={{ height: 50, mb: 3, borderRadius: 3 }} animation="wave" variant="rectangular" />
-                <Skeleton sx={{ height: 190, mb: 3, borderRadius: 3 }} animation="wave" variant="rectangular" />
-            </>
-        )
-    }
+  if (loading) {
     return (
-        <React.Fragment>
-            <Paper>
-                <Table sx={{ mb: 3 }}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Price</TableCell>
-                            <TableCell>Price(Rials)</TableCell>
-                            <TableCell>Chart</TableCell>
-                        </TableRow>
-                    </TableHead>
-                </Table>
-            </Paper>
-            <Paper sx={{ mb: 3 }}>
-                <Table sx={{ mb: 3 }}>
-                    <TableBody>
-                        {(symbolprice > 0
-                            ? symbolprice.slice(page * rowperpage, page * rowperpage + rowperpage)
-                            : symbolprice
-                        ).map((symbol, index) => (
-                            <TableRow key={index}>
-                                <TableCell>{symbol.name}</TableCell>
-                                <TableCell>{numberWithCommas(symbol.price[9])}</TableCell>
-                                <TableCell>{numberWithCommas(symbol.price[9] * 50000)} Rial</TableCell>
-                                <TableCell>
-                                    <Chart
-                                        {...chartdata}
-                                        series={series}
-                                    />
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-                <Stack spacing={2} alignItems={'center'} padding={2}>
-                    <Pagination count={symbols.length} variant="outlined" shape="rounded" page={page} onChange={handleChangePage} />
-                </Stack>
-            </Paper>
-        </React.Fragment>
+      <>
+        <Skeleton
+          sx={{ height: 50, mb: 3, borderRadius: 3 }}
+          animation="wave"
+          variant="rectangular"
+        />
+        <Skeleton
+          sx={{ height: 190, mb: 3, borderRadius: 3 }}
+          animation="wave"
+          variant="rectangular"
+        />
+      </>
     );
+  }
+
+  return (
+    <>
+      <Paper>
+        <Table sx={{ mb: 3 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Price(Rials)</TableCell>
+              <TableCell>Chart</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </Paper>
+      <Paper sx={{ mb: 3 }}>
+        <Table sx={{ mb: 3 }}>
+          <TableBody>
+            {symbolprice.map((symbol, index) => (
+              <TableRow key={index}>
+                <TableCell>{symbol.name}</TableCell>
+                <TableCell>
+                  {numberWithCommas(parseFloat(symbol.price[49]).toFixed(2))}
+                </TableCell>
+                <TableCell>
+                  {numberWithCommas(parseInt(symbol.price[49] * 500000))} Rial
+                </TableCell>
+                <TableCell>
+                  <Chart
+                    {...chartdata}
+                    series={[{ name: symbol.name, data: symbol.price }]}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Stack spacing={2} alignItems={"center"} padding={2}>
+          <Pagination
+            count={Math.ceil(symbols.length / rowsPerPage)}
+            variant="outlined"
+            shape="rounded"
+            page={page}
+            onChange={handleChangePage}
+          />
+        </Stack>
+      </Paper>
+    </>
+  );
 }
